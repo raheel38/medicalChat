7af7c20413ab4da37470c10197bf7de739eb66ce
Rahila <rahilaahmadi38@gmail.com>
2025-07-14 11:31:25 +0900

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medicalchat',
+    version='0.1.0',
+    author='Rahila Ahmadi',
+    author_email='rahilaahmadi38@.com',
+    packages=find_packages(),
+    install_requires=[]
+)
